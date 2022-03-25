e1fc94f91b76678272466341a5524317269ac719
ErikBoesen <me@erikboesen.com>
2022-03-24 23:56:47 -0400

    Fix date parameters

--- lib/yalemenus.rb
+++ lib/yalemenus.rb
@@ -1,12 +1,13 @@
 require "net/http"
 require "uri"
 require "json"
+require "date"
 
 class YaleMenus
 
     @@API_ROOT = "https://api.yalemenus.com/"
 
-    def self.get(path, params: nil)
+    def self.get(path, params = nil)
         uri = URI(@@API_ROOT + path)
         unless params.nil?
             uri.query = URI.encode_www_form(params)
@@ -14,6 +15,13 @@ class YaleMenus
 
         res = Net::HTTP.get_response(uri)
         return JSON.parse(res.body)
+    end
+
+    def self.format_date(date)
+        if date.is_a? Date
+          return date.strftime('%Y-%m-%d')
+        end
+        return date
     end
 
     def self.status()
@@ -35,13 +43,13 @@ class YaleMenus
     def self.hall_meals(hall_id, date: nil, start_date: nil, end_date: nil)
         params = {}
         unless date.nil?
-            params[:date] = date
+            params[:date] = format_date(date)
         end
         unless start_date.nil?
-            params[:start_date] = start_date
+            params[:start_date] = format_date(start_date)
         end
         unless end_date.nil?
-            params[:end_date] = end_date
+            params[:end_date] = format_date(end_date)
         end
         get("halls/#{hall_id}/meals", params)
     end
