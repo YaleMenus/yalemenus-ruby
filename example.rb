cdca16f45e252a8dceec3a3f04ac2090700ae704
ErikBoesen <me@erikboesen.com>
2022-03-24 23:56:30 -0400

    Expand example

--- example.rb
+++ example.rb
@@ -8,6 +8,12 @@ puts "There are #{halls.length} dining halls available."
 hall = YaleMenus.hall('GH')
 puts "The Grace Hopper dining hall is located at #{hall['address']}."
 
+meals = YaleMenus.hall_meals('JE', date: Date.new(2022, 2, 24))
+puts "There are #{meals.length} meals being served on 2022-02-24."
+
+items = YaleMenus.meal_items(meals[0]['id'])
+puts "The first item in the first of those meals is #{items[0]['name']}."
+
 buttery = YaleMenus.buttery('GH')
 puts "The Grace Hopper buttery has the following data:"
 puts buttery
