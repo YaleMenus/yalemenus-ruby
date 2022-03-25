34993440c712499fa40e51a99750d02db261e655
ErikBoesen <me@erikboesen.com>
2022-03-24 23:03:27 -0400

    Add to example

--- example.rb
+++ example.rb
@@ -7,3 +7,7 @@ puts "There are #{halls.length} dining halls available."
 
 hall = YaleMenus.hall('GH')
 puts "The Grace Hopper dining hall is located at #{hall['address']}."
+
+buttery = YaleMenus.buttery('GH')
+puts "The Grace Hopper buttery has the following data:"
+puts buttery
